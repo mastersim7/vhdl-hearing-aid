8f175b2c86ea1d43bff6aaf62bb7020a55f769d1
shwan.ciyako <shwan.ciyako@94776321-b6cd-9268-6af4-086d0e89f964>
2011-02-28 18:19:49 +0000

    added a converter for matlab coeff

---
+++ trunk/matlab/eq/pre_design/bitconvert.m
@@ -0,0 +1,20 @@
+magnitude = 23
+greatest = 2^ 23 
+x=0.6
+y = x * greatest
+y=round(y);
+
+val = dec2bin(mod((y),2^24),24)
+
+
+
+
+% 
+% str = dec2bin(y,23)
+% zeros = dec2bin(0,1)
+% ones = dec2bin(1,1)
+%   
+% str1= [zeros str]
+% 
+% str1+str1
+%   
