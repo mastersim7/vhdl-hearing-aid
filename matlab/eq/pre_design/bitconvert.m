80c5c5ff2c1165875b4dd842869fa99b3f158603
shwan.ciyako <shwan.ciyako@94776321-b6cd-9268-6af4-086d0e89f964>
2011-03-01 15:24:54 +0000

    matlab code is good now and the coeff test can be pasted strait into the coeff_IO.vhd

--- matlab/eq/pre_design/bitconvert.m
+++ matlab/eq/pre_design/bitconvert.m
@@ -3,7 +3,7 @@ greatest = 2^ 23
 n = 111;
 for m=1:8 ,
 for i=1:n ,
-y = b1(i) * greatest;
+y = b{m}(i) * greatest;
 y=round(y);
 l{m,i} = dec2bin(mod((y),2^24),24);
 end;
@@ -13,13 +13,13 @@ end;
 %now let's write them to a text file 
 fid=fopen('coeff.txt','wt');
 [rows,cols]=size(l);
-
-for i=1:cols
+for m=1:rows,
+for i=1:cols,
 %fprintf(fid,'%s,',l{i,1:end})
-fprintf(fid,'CO(1,1)=%s;\t',l{1:rows,i});
+fprintf(fid,'tc(%i,%i)<="%s";', m, i, l{m,i});
 fprintf(fid,'\n');
 end
-
+end
 fclose(fid);
 
 
--- matlab/eq/pre_design/filterB3.m
+++ matlab/eq/pre_design/filterB3.m
@@ -15,15 +15,15 @@ Fs = 22300;  % Sampling Frequency
 N    = 220;      % Order220
 
 
-FcB11=	1% First Cutoff Frequency
-FcB12=	(1e4)/(2^7)% Second Cutoff Frequency
-FcB2=   (1e4)/(2^6)
-FcB3=   (1e4)/(2^5)
-FcB4=   (1e4)/(2^4)
-FcB5=   (1e4)/(2^3)
-FcB6=   (1e4)/(2^2) 
-FcB7=   (1e4)/2
-FcB8=   (1e4)
+FcB11=	100% First Cutoff Frequency
+FcB12=	100+ ((1e4)/(2^7))% Second Cutoff Frequency
+FcB2=   100+ ((1e4)/(2^6))
+FcB3=   100+ ((1e4)/(2^5))
+FcB4=   100+ ((1e4)/(2^4))
+FcB5=   100+ ((1e4)/(2^3))
+FcB6=   100+ ((1e4)/(2^2))
+FcB7=   100+ ((1e4)/2)
+FcB8=   100+ ((1e4))
 
 
 
@@ -36,37 +36,37 @@ win = hamming(N+1);
 %win = nuttallwin(N+1);
 
 % Calculate the coefficients using the FIR1 function.
-b1  = fir1(N, [FcB11 FcB12]/(Fs/2), 'bandpass', win, flag);
+b{1}  = fir1(N, [FcB11 FcB12]/(Fs/2), 'bandpass', win, flag);
 % tested a highpass b1  = fir1(N, 100/(Fs/2), 'high', win, flag);
 
-Hd1 = dfilt.dffir(b1);
-b2  = fir1(N, [FcB12 FcB2]/(Fs/2), 'bandpass', win, flag);
-Hd2 = dfilt.dffir(b2);
-b3  = fir1(N, [FcB2 FcB3]/(Fs/2), 'bandpass', win, flag);
-Hd3 = dfilt.dffir(b3);
-b4  = fir1(N, [FcB3 FcB4]/(Fs/2), 'bandpass', win, flag);
-Hd4 = dfilt.dffir(b4);
-b5  = fir1(N, [FcB4 FcB5]/(Fs/2), 'bandpass', win, flag);
-Hd5 = dfilt.dffir(b5);
-b6  = fir1(N, [FcB5 FcB6]/(Fs/2), 'bandpass', win, flag);
-Hd6 = dfilt.dffir(b6);
-b7  = fir1(N, [FcB6 FcB7]/(Fs/2), 'bandpass', win, flag);
-Hd7 = dfilt.dffir(b7);
-b8  = fir1(N, [FcB7 FcB8]/(Fs/2), 'bandpass', win, flag);
-Hd8 = dfilt.dffir(b8);
-Hd9 = dfilt.dffir (b1+b2+b3+b4+b5+b6+b7+b8); %
+Hd1 = dfilt.dffir(b{1});
+b{2}  = fir1(N, [FcB12 FcB2]/(Fs/2), 'bandpass', win, flag);
+Hd2 = dfilt.dffir(b{2});
+b{3}  = fir1(N, [FcB2 FcB3]/(Fs/2), 'bandpass', win, flag);
+Hd3 = dfilt.dffir(b{3});
+b{4}  = fir1(N, [FcB3 FcB4]/(Fs/2), 'bandpass', win, flag);
+Hd4 = dfilt.dffir(b{4});
+b{5}  = fir1(N, [FcB4 FcB5]/(Fs/2), 'bandpass', win, flag);
+Hd5 = dfilt.dffir(b{5});
+b{6}  = fir1(N, [FcB5 FcB6]/(Fs/2), 'bandpass', win, flag);
+Hd6 = dfilt.dffir(b{6});
+b{7}  = fir1(N, [FcB6 FcB7]/(Fs/2), 'bandpass', win, flag);
+Hd7 = dfilt.dffir(b{7});
+b{8}  = fir1(N, [FcB7 FcB8]/(Fs/2), 'bandpass', win, flag);
+Hd8 = dfilt.dffir(b{8});
+Hd(9) = dfilt.dffir (b{1}+b{2}+b{3}+b{4}+b{5}+b{6}+b{7}+b{8}); %
 %added up the filters here to see
-    [H1 W1] = freqz(b1,1);
+    [H1 W1] = freqz(b{1},1);
     
-    [H2 W2] = freqz(b2,1);
-    [H3 W3] = freqz(b3,1);
-    [H4 W4] = freqz(b4,1);
-    [H5 W5] = freqz(b5,1);
-    [H6 W6] = freqz(b6,1);
-    [H7 W7] = freqz(b7,1);
-    [H8 W8] = freqz(b8,1);
+    [H2 W2] = freqz(b{2},1);
+    [H3 W3] = freqz(b{3},1);
+    [H4 W4] = freqz(b{4},1);
+    [H5 W5] = freqz(b{5},1);
+    [H6 W6] = freqz(b{6},1);
+    [H7 W7] = freqz(b{7},1);
+    [H8 W8] = freqz(b{8},1);
     
-    [H9 W9] = freqz(b1+b2+b3+b4+b5+b6+b7+b8); %b2+b3+b4
+    [H9 W9] = freqz(b{1}+b{2}+b{3}+b{4}+b{5}+b{6}+b{7}+b{8}); %b2+b3+b4
     %added up the filters here to see hjow they look like in the bank
     
 % [EOF]
@@ -81,4 +81,4 @@ plot(W7*Fs/2/pi, 20*log10(abs(H7)));
 plot(W8*Fs/2/pi, 20*log10(abs(H8)));
 plot(W9*Fs/2/pi, 20*log10(abs(H9)));
 
-fvtool (Hd1,Hd2,Hd3,Hd4,Hd5,Hd6,Hd7,Hd9)%
+fvtool (Hd1,Hd2,Hd3,Hd4,Hd5,Hd6,Hd7)%,Hd9)%
