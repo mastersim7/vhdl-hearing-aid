6ca36d678c02e142b315e5ae11c82745968ec88d
shwan.ciyako <shwan.ciyako@94776321-b6cd-9268-6af4-086d0e89f964>
2011-03-03 10:14:42 +0000

    more lines added into filterblock to make the state machine work, I'm thinking of chaning the data_buffer;)

--- trunk/eq/EQ_data_type.vhd
+++ trunk/eq/EQ_data_type.vhd
@@ -4,6 +4,7 @@ USE ieee.numeric_std.ALL;
 Package EQ_data_type IS 
 	GENERIC(L:NATURAL := 12);	 
     TYPE taps_type IS ARRAY (1 to 8, 1 TO 111) OF STD_LOGIC_VECTOR( 23 downto 0 );
+    TYPE gain_type IS ARRAY (1 to 8) OF STD_LOGIC_VECTOR( 23 downto 0 );
     TYPE sample IS  STD_LOGIC_VECTOR( L-1 DOWNTO 0 );
     TYPE Multi_Result IS STD_LOGIC_VECTOR( (2*L)-1 DOWNTO 0 );
     TYPE Multi_Result_Array is ARRAY (3 downto 0) of Multi_Result;
