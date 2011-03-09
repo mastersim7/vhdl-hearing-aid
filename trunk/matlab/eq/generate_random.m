1e4da5a1c0d7e111241c1e5ce6790f7bfd4fac05
lundell.mathias@gmail.com <lundell.mathias@gmail.com@94776321-b6cd-9268-6af4-086d0e89f964>
2011-03-09 15:15:49 +0000

    Created matlab functions for creating random test vectors and get them as strings of signed binary. generate_random.m and dec2signedbinary.m

--- trunk/matlab/eq/generate_random.m
+++ trunk/matlab/eq/generate_random.m
@@ -1,17 +1,23 @@
 function num = generate_random(n, signed)
 
 % Generate random numbers
+if rand(1,1) > 0.5
+    neg = 1;
+else
+    neg = -1;
+end
+
 if signed == 1
     max = 2^(n-1)-1;
 
-    num = max*rand(1,1);
+    num = neg*max*rand(1,1);
 
     if round(num) == max
         num = num-1;
     end
 else
     max = 2^n;
-    num = max*(rand(1,1) + 1)/2;
+    num = neg*max*rand(1,1);
 end
 
 num = round(num);
--- trunk/matlab/eq/generate_test_vectors.m
+++
@@ -1,4 +0,0 @@
-% Generate test vectors
-clc;
-n = 12;
-v1 = generate_random(n, 1) % signed = true
