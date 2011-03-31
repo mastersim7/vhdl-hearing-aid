c214610cf8f8300217a0f69015e58118d1a6546b
shwan.ciyako <shwan.ciyako@94776321-b6cd-9268-6af4-086d0e89f964>
2011-03-31 09:42:23 +0000

    no changs just cleaned up the code

--- new_eq/average_if.vhd
+++ new_eq/average_if.vhd
@@ -6,9 +6,9 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
---USE ieee.std_logic_signed.ALL;
 USE work.EQ_data_type.ALL;
 USE work.EQ_functions.ALL;
+
 
 ENTITY average_if IS
     GENERIC(
@@ -21,10 +21,12 @@ ENTITY average_if IS
             CE      : IN STD_LOGIC;
             OE_GAINAMP : IN STD_LOGIC;
             REQ     :IN STD_LOGIC;
-            Gained_Samples: IN Gained_result_Array_16; --an 8 array of 16 bit vectors
+            Gained_Samples: IN Gained_result_Array_16; --an 8-array of 16 bit vectors
             OE      : OUT STD_LOGIC; 
             Q       : OUT Gained_result_Array_16);
 END;
+
+
 ARCHITECTURE average_if_arch OF average_if IS
     signal started : std_logic;
 
@@ -32,65 +34,42 @@ BEGIN
 
 PROCESS(clk, CE)
     VARIABLE Gained_Samples_var: Gained_result_Array_16;
-    
     VARIABLE i,m : INTEGER;
 BEGIN
     IF clk'EVENT AND clk = '1' THEN
+
 	IF reset ='1' THEN 
 	   FOR k IN 1 TO 8 LOOP
-          Q(k) <= (OTHERS => '0');
-     END LOOP;
-	    i:=0;
-	   -- m:=1; -- array starts from 1
-	    OE<='0';
- 	    started <='0';
+	   Q(k) <= (OTHERS => '0');
+	   END LOOP;
+	   i:=0;
+	   OE<='0';
+ 	   started <='0'; 
 	ELSE
-      IF CE = '1' THEN --slower clock
-           
-	    IF started = '1' THEN 
-	
-      --
-  
-      --IF REQ  = '1' THEN 
-              
-
-      IF OE_GAINAMP = '1' THEN 
-      IF i /= NUM_OF_SAMPLES THEN 
-				      OE<='0';
-				      
-              --IF m /= (NUM_OF_BANDS+1) THEN 
-             
-              FOR k IN 1 TO NUM_OF_BANDS LOOP
-                Gained_Samples_var(k) := STD_LOGIC_VECTOR(SIGNED(Gained_Samples(k))+SIGNED(Gained_Samples_var(k)));
-              END LOOP;
-             
-              -- Gained_Samples_var(m) := if_adder(Gained_Samples(m),Gained_Samples_var(m));
-               
-                --m:=m+1;
-            
-              --ELSE
-                --m:=1;
-                i := i+1;
-              --end IF; --m
-              
-      ELSE 
-                 Q<=Gained_Samples_var;
-                 
-	             FOR k IN 1 TO 8 LOOP
-                 Gained_Samples_var(k) := (OTHERS => '0');
-                END LOOP;
-                 OE<='1';
-                 i := 0;
-                 started <='0'; 
-        END IF;--i
-      
-        END IF; --OE_GSINSMP
-       ELSE
-      ---started <=OE_GAINAMP ;  this one should wait for req
-      started <= REQ;
-      END IF; --started
-          END IF; --CE
-     END IF; --reset
-    END IF; --clk
-	 End Process;
+		IF CE = '1' THEN --slower clock
+			IF started = '1' THEN -- makes sure the entire calculation is being done once per req signal
+				IF OE_GAINAMP = '1' THEN  -- waits until the gain is multiplied to the output and the output updated
+					IF i /= NUM_OF_SAMPLES THEN
+					      OE<='0';
+					      FOR k IN 1 TO NUM_OF_BANDS LOOP -- parallel of 8 additions ?
+				              	Gained_Samples_var(k) := STD_LOGIC_VECTOR(SIGNED(Gained_Samples(k))+SIGNED(Gained_Samples_var(k)));
+				              END LOOP;
+				              i := i+1;
+				        ELSE
+				              Q<=Gained_Samples_var; -- the entire array gets updated per one clock
+                 			      OE<='1';
+                 			      i := 0;
+                 			      started <='0';  -- turn off this component until next req
+                 			      FOR k IN 1 TO 8 LOOP -- zero the temp variable
+                 			      	Gained_Samples_var(k) := (OTHERS => '0');
+                 			      END LOOP;
+				        END IF;--i
+			        END IF; --OE_GSINSMP
+		       ELSE --STARTED
+		       started <= REQ;
+		       END IF; --started
+		END IF; --CE
+        END IF; --reset
+    END IF; --cl
+END PROCESS;
 END ARCHITECTURE;
