6bc64e22e7d1a66521b409890280764108ea07b1
lundell.mathias <lundell.mathias@gmail.com>
2011-04-04 12:13:15 +0000

    Added file bin2int.m

--- matlab/eq/pre_design/bitconvert.m
+++ matlab/eq/pre_design/bitconvert.m
@@ -2,7 +2,7 @@
 %greatest = (2^ magnitude-1) -1
 %n = 111;
 % Coefficients
-coeff_num = 40; % number of randomly generated coefficients
+coeff_num = 79; % number of randomly generated coefficients
 coeff_int = 0;
 coeff_frac = 0.0;
 coeff_bin = '';  
@@ -13,7 +13,8 @@ for m=1:8
     for i=1:coeff_num
         y = b{m}(i) * coeff_max;
         y=round(y)
-        l{m,i} = int2bin(y, 24)       
+        l{i,m} = int2bin(y, 24)
+        
         % testing 
         
     end;
@@ -22,7 +23,7 @@ end;
 %now let's write them to a text file 
 fid=fopen('coeff.txt','wt');
 [rows,cols]=size(l);
-for m=1:rows, %rows,
+for m=1:rows,
 for i=1:cols,
 %fprintf(fid,'%s,',l{i,1:end})
 %fprintf(fid,'tc(%i,%i)<="%s";', m, i, l{m,i});
