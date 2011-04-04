d85d47483b9601a7b138f5a026dbca981c4dba9b
lundell.mathias@gmail.com <lundell.mathias@gmail.com@94776321-b6cd-9268-6af4-086d0e89f964>
2011-04-04 12:06:42 +0000

    Modified eq_test.m

--- trunk/matlab/eq/eq_test.m
+++ trunk/matlab/eq/eq_test.m
@@ -1,15 +1,21 @@
 % eq_test.m
 % Alexey Sidelnikov, Mathias Lundell
-% 2011-03-17
+
+% 2011-04-01
+% Reads coefficients for a single filter from an excel-file.
+% See OBS lower in document
 
 % 2011-03-21
 % Added commands for exporting values to excel.
 
-sign = 1;          % want signed bit vectors from 'generate_random()'
+% 2011-03-17
+% First version created.
+clc;
+sign = 1;  % want signed bit vectors from 'generate_random()'
 
 % Samples
 samples_n = 12;    % number of bits in samples
-samples_num = 220; % number of randomly generated samples
+samples_num = 80; % number of randomly generated samples
 samples_int = 0; 
 samples_frac = 0.0;
 samples_bin = ''; 
@@ -23,7 +29,7 @@ summed_samples_n = 13; % Sum should be 13 bits (two samples added together)
 summed_samples_max = 2^(summed_samples_n-1)-1;
 
 % Coefficients
-coeff_num = 110; % number of randomly generated coefficients
+coeff_num = 40; % number of randomly generated coefficients
 coeff_int = 0;
 coeff_frac = 0.0;
 coeff_bin = '';  
@@ -36,6 +42,11 @@ coeff_x_summed_samples_frac = 0.0;
 coeff_x_summed_samples_bin = ''; 
 coeff_x_summed_samples_n = 37; % Result should be 37 bits
 coeff_x_summed_samples_max = 2^(coeff_x_summed_samples_n-1)-1;
+
+%%%%%%%%%% OBS %%%%%%%%%%%%%%%%
+% Need to have binary values of 37 bit for output from single filter
+% accumulated together
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Output values
 pre_output_n = 40;  % result of adding all the results of multiplications, should be 40 bits   
@@ -51,6 +62,7 @@ output_max = 2^(output_n-1)-1;
 % Generate array of samples:
 for i = 1 : samples_num
     samples_int(i) = generate_random(samples_n, sign);
+    samples_frac(i) = samples_int(i)/samples_max;
     samples_bin(i,:) = int2bin(samples_int(i), samples_n);
 end
 
@@ -62,8 +74,11 @@ for i = 1 : (samples_num/2)
 end
 
 % Generate array of coefficients:
+% Read coefficients from file...
+[num, coeff_bin_cell, raw] = xlsread('Coeffs.xls', 'Sheet1', 'A2:A41');
 for i = 1 : coeff_num
-    coeff_int(i) = generate_random(coeff_n, sign);
+    coeff_int(i) = bin2int(char(coeff_bin_cell(i)));
+    %coeff_int(i) = generate_random(coeff_n, sign);
     coeff_frac(i) = coeff_int(i)/coeff_max;
     coeff_bin(i,:) = int2bin(coeff_int(i), coeff_n);
 end
@@ -81,25 +96,9 @@ end
 % Add all the results of multiplications together:
 pre_output_int = sum(coeff_x_summed_samples_int);
 pre_output_bin = int2bin(pre_output_int, pre_output_n);
-
-% pre_output_frac = pre_output_int/coeff_x_summed_samples_max; 
-% <- Maybe this is wrong? Changed below. /Alexey-13:35-110317
 pre_output_frac = pre_output_int/pre_output_max;
 
-% ! At this point we have the output from a single filter. I think we can test a filter
-%   with this and then we have to decide to either keep the 37 bits until we have summed
-%   the output from all eight filters. But let's say we cut the binary numbers to 12 bits and
-%   evaluate that case (for the sake of it). 
-%   /Mathias-08:30-110317
-
 % Cut 'pre_output' in 12 bits and get the final output:
-
-% Alexey's old variant:
-% output_int = pre_output_int;
-% for i = 1 : output_n
-% output_bin(i) = pre_output_bin(i);
-% end
-
 steps_to_right_shift = pre_output_n-output_n;
 output_int = floor(pre_output_int/(2^steps_to_right_shift));
 output_bin = int2bin(output_int, output_n);
@@ -120,37 +119,58 @@ output_frac = output_int/output_max;
 fid_samples = fopen('samples.tv', 'w');
 fid_coeff = fopen('coeff.tv', 'w');
 fid_output = fopen('output.tv', 'w');
+fid_pre_output = fopen('pre_output.tv', 'w');
+
 % Writing values in files
-fprintf(fid_samples, '%c%c%c%c%c%c%c%c%c%c%c%c\n', samples_bin);
-fprintf(fid_coeff, '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n', coeff_bin);
-fprintf(fid_output, '%s', output_bin);
+fprintf(fid_samples, '%c%c%c%c%c%c%c%c%c%c%c%c\n', samples_bin');
+fprintf(fid_coeff, '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n', coeff_bin');
+fprintf(fid_output, '%s', output_bin');
+fprintf(fid_pre_output, '%s', pre_output_bin');
+
 % Closing files
 fclose(fid_samples);
 fclose(fid_coeff);
 fclose(fid_output);
-
+fclose(fid_pre_output);
 
 % Export result to excel
-text = {'Samples', 'Coeff fraction', 'Y fraction', ...
-    'Test fraction', 'Sample integer', 'Coeff integer', ...
-    'Y integer', 'Test integer', '', 'Sample binary', 'Coeff binary', 'Y binary'};
-xlswrite('test_vectors_multiply.xls', text, 'Generated test vector values', 'A1');
+text = {'Samples', 'Coeff fraction', 'coeff_x_summed_samples_frac', ...
+    'pre_output_frac', ... 
+    'Sample integer', 'Coeff integer', 'coeff_x_summed_samples_int', ...
+    'pre_output_int', ...
+    'Sample binary', 'Coeff binary', 'coeff_x_summed_samples_bin', ...
+    'pre_output_bin'};
+xlswrite('eq_test.xls', text, 'Sheet1', 'A1');
 
-temp = sample_frac';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'A2');
+temp = samples_frac';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'A2');
 temp = coeff_frac';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'B2');
-temp = y_frac';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'C2');
+xlswrite('eq_test.xls', temp, 'Sheet1', 'B2');
+temp = coeff_x_summed_samples_frac';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'C2');
+temp = pre_output_frac';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'D2');
 
-temp = sample_int';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'E2');
+temp = samples_int';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'E2');
 temp = coeff_int';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'F2');
-temp = y_int';
-xlswrite('test_vectors_multiply.xls', temp, 'Generated test vector values', 'G2');
-% COMMENTS:
-%
-% 2) I'm also not sure about 40 bits for 'pre_output'.
-% 3) Do we need to export results to excel?
-% /Alexey-04:30-110317
+xlswrite('eq_test.xls', temp, 'Sheet1', 'F2');
+temp = coeff_x_summed_samples_int';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'G2');
+temp = pre_output_int';
+xlswrite('eq_test.xls', temp, 'Sheet1', 'H2');
+
+temp = cellstr(samples_bin);
+xlswrite('eq_test.xls', temp, 'Sheet1', 'I2');
+temp = cellstr(coeff_bin);
+xlswrite('eq_test.xls', temp, 'Sheet1', 'J2');
+temp = cellstr(coeff_x_summed_samples_bin);
+xlswrite('eq_test.xls', temp, 'Sheet1', 'K2');
+temp = cellstr(pre_output_bin);
+xlswrite('eq_test.xls', temp, 'Sheet1', 'L2');
+
+% Temporarily lines, write coefficients so we can copy paste 
+% them easily to vhdl-file.
+fid_coeff = fopen('coefficients.txt', 'w');
+fprintf(fid_coeff, '"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",', coeff_bin');
+fclose(fid_coeff);
