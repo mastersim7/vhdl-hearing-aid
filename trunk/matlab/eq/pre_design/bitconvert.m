ba3dc77985d838fd4f13517b9283444ef93e9689
shwan.ciyako <shwan.ciyako@94776321-b6cd-9268-6af4-086d0e89f964>
2011-04-11 13:08:59 +0000

    changed the word lenght

--- trunk/matlab/eq/pre_design/bitconvert.m
+++ trunk/matlab/eq/pre_design/bitconvert.m
@@ -2,18 +2,18 @@
 %greatest = (2^ magnitude-1) -1
 %n = 111;
 % Coefficients
-coeff_num = 79; % number of randomly generated coefficients
+coeff_num = 40; % number of randomly generated coefficients
 coeff_int = 0;
 coeff_frac = 0.0;
 coeff_bin = '';  
-coeff_n = 24;    % Coefficients should be 24 bits
+coeff_n = 13;    % Coefficients should be 24 bits
 coeff_max = 2^(coeff_n-1)-1;
 
 for m=1:8
     for i=1:coeff_num
         y = b{m}(i) * coeff_max;
         y=round(y)
-        l{i,m} = int2bin(y, 24)
+        l{m,i} = int2bin(y, coeff_n)
         
         % testing 
         
@@ -22,7 +22,7 @@ end;
 
 %now let's write them to a text file 
 fid=fopen('coeff.txt','wt');
-[rows,cols]=size(l);
+[rows,cols]=size(l)
 for m=1:rows,
 for i=1:cols,
 %fprintf(fid,'%s,',l{i,1:end})
