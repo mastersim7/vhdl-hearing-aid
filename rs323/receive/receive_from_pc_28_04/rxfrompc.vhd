1d00c21f57b5e2640d16d282e8152b04496302ec
amitkulkarnis <amitkulkarnis@gmail.com>
2011-05-02 14:37:54 +0000

    added a control signal fla_tx to receive the average gain level at proper times, this control signal os controlled in RS232API

--- rs323/receive/receive_from_pc_28_04/rxfrompc.vhd
+++ rs323/receive/receive_from_pc_28_04/rxfrompc.vhd
@@ -1,14 +1,18 @@
--------------------------------------------------------------------------------------------------------------------|
--- Design Name: Receive Data serially from PC to FPGA via RS232 COM port                                           |
--- Module Name: HIF_RS232_Receive_from_PC - Behavioral                                                             |
--- Designer: Amit Kulkarni(amitk@student.chalmers.se)                                                              |
--- Date: 2011-03-19                                                                                                |
--------------------------------------------------------------------------------------------------------------------|
--- HISTORY :                                                                                                       |
---Author : Amit Kulkarni                                                                                           |
---Date : 2011-04-04                                                                                                |
---Changes made : Changes in the clock division to set the baud rate properly and adding RS232_enable signal        |
--------------------------------------------------------------------------------------------------------------------|
+------------------------------------------------------------------------------------------------------------------------------|
+-- Design Name: Receive Data serially from PC to FPGA via RS232 COM port                                                      |
+-- Module Name: HIF_RS232_Receive_from_PC - Behavioral                                                                        |
+-- Designer: Amit Kulkarni(amitk@student.chalmers.se)                                                                         |
+-- Date: 2011-03-19                                                                                                           |
+------------------------------------------------------------------------------------------------------------------------------|
+-- HISTORY :                                                                                                                  |
+--Author : Amit Kulkarni                                                                                                      |
+--Date : 2011-04-04                                                                                                           |
+--Changes made : Changes in the clock division to set the baud rate properly and adding RS232_enable signal                   |
+------------------------------------------------------------------------------------------------------------------------------|                                                                                                   |
+--Author : Amit Kulkarni                                                                                                      |
+--Date : 2011-05-02                                                                                                           |
+--Changes made : added an control signal to instruct when to send the average gain levels to the pc(dependent on the RS232API)|       |
+------------------------------------------------------------------------------------------------------------------------------|
 ----------------------------------------------------------------------------------
 -- Design Name: Receive Datas from PC to FPGA
 -- Module Name:    receive10_2 - Behavioral 
@@ -24,7 +28,9 @@ GENERIC( Serial_word_length : NATURAL :=10); --receiving 10 bits start+8 bits of
    PORT( System_clk_Rx : IN STD_LOGIC;
          Rx: IN STD_LOGIC;
          reset_rx : IN STD_LOGIC;
-		 Gain_array :OUT Gained_result_Array
+         --OE_Tx : IN STD_LOGIC; --Flag sent by the Equalizer conveying that data filling into 'gain_array_output' is finished
+         flag_Tx : OUT STD_LOGIC; --flag to indicate that Eqaulizer can now send the average gain signals
+         Gain_array :OUT Gained_result_Array
         -- temp_led:OUT STd_LOGIC_vector(7 downto 0)
        );
 END HIF_RS232_Receive_from_PC;
@@ -46,12 +52,11 @@ PROCESS(System_clk_Rx,reset_rx)
     VARIABLE k : NATURAL RANGE 0 TO 8;
     VARIABLE memory_temp : Gained_result_Array_8;
     VARIABLE LUT : Gained_result_Array;
+    VARIABLE rx_flag_int : STD_LOGIC;
 BEGIN
 IF(reset_rx = '1') THEN
-           FOR k IN 0 TO 7 LOOP
-	       Gain_array(k) <="0000000000001";
-	       END LOOP;
-           
+            Gain_array <= (OTHERS => "0000000000001");
+            rx_flag_int := '0';
             counter_clk_div <= 0;            
             --startbit <= '1';
             k := 0;
@@ -59,16 +64,17 @@ IF(reset_rx = '1') THEN
             enable := '0';    
 ELSIF(reset_rx = '0') THEN
     IF(rising_edge(system_clk_Rx)) THEN
-        IF( startbit = '0' OR enable = '1' )THEN        -- to start new reception or complete one comple byte transfer
+        IF( startbit = '0' OR enable = '1' )THEN  -- to start new reception or complete one comple byte transfer
             enable := '1';
+            --rx_flag_int := '0';
             counter_clk_div <= counter_clk_div+1;
-			IF(counter_clk_div = 5207)THEN                                   -- this clock count corresponds to 50MHz for 9600 bps and can be changed if we are working with 1MHz or any  other frequency
+                        IF(counter_clk_div = 5207)THEN   -- this clock count corresponds to 50MHz for 9600 bps and can be changed if we are working with 1MHz or any  other frequency
                 counter_clk_div <= 0;
                 IF(j < Serial_word_length )THEN
-                    receive(j) <= Rx;                           -- receiving at 9660 bps and storing it, serial bits are packed
+                    receive(j) <= Rx;    -- receiving at 9660 bps and storing it, serial bits are packed
                     j <= j+1;
                     IF ( j = Serial_word_length-2 ) THEN
-                        memory_temp(k) := receive( Serial_word_length-3 DOWNTO 0);       -- packed  datas are sent to receive_out(for testing counter_clk_div mapped this receive out eight bits into LED)
+                        memory_temp(k) := receive( Serial_word_length-3 DOWNTO 0);  -- packed  datas are sent to receive_out(for testing counter_clk_div mapped this receive out eight bits into LED)
                         CASE memory_temp(k) IS -- Look Up Table begins here(as data are not decided so initially we set all the bit equal to high)
                             WHEN "00000001" => 
                                 LUT(k) :="0000000000011"; 
@@ -134,6 +140,8 @@ ELSIF(reset_rx = '0') THEN
                                 LUT(k) :="0110010110100"; 
                             WHEN "00100000" => 
                                 LUT(k) :="0111111111111"; 
+                            WHEN "01000000" =>		
+                                rx_flag_int := '1';
                             WHEN OTHERS => 
                                 NULL;
                         END CASE;
@@ -150,7 +158,7 @@ ELSIF(reset_rx = '0') THEN
                             k := 0; -- Once all LUT value is given restart from the first block
                         END IF;                              
                         --k := k +1;
-                        enable := '0';                        --when one byte of data received the reception is disabled but again when a new start bit is encounteredd again the reception starts
+                        enable := '0'; --when one byte of data received the reception is disabled but again when a new start bit is encounteredd again the reception starts
                         j <= 0;
 --                        IF(k = 8) THEN                       
 --                            k := 0;
@@ -161,10 +169,10 @@ ELSIF(reset_rx = '0') THEN
         END IF;        
     END IF;
 END IF;
+
+flag_Tx  <= rx_flag_int;
+rx_flag_int := '0';    
 --temp_led <= LUT(7)(12 downto 5);
 END PROCESS;
 
 END Behavioral;
-
-
-                    
