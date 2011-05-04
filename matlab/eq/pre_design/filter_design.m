745f88839740492ac22d8263add7a9410021703a
lundell.mathias <lundell.mathias@gmail.com>
2011-05-04 14:14:20 +0000

    added some different tests with different number of coefficients

--- matlab/eq/pre_design/filter_design.m
+++ matlab/eq/pre_design/filter_design.m
@@ -11,23 +11,25 @@ fs = 22.3e3;
 Nyquist = fs/2;
 
 % Define order for all filters:
-filter_order = [201 201 201 201 151 61 31 21];
+%filter_order = [211 211 211 211 157 127 31 15];
+%filter_order = [64 64 64 64 64 64 64 64];
+filter_order = [199 199 199 199 199 199 199 199];
 
 % Define weights
-filter_weight = [1 1 1 1 1.3 1 1 1];
+filter_weight = [1 1 1 1 1 1 1 1];
 
 % Cut-off frequencies
-fc = 1e4*1./[2^7, 2^6, 2^5, 2^4, 2^3, 2^2, 2^1, 0.9];
+fc = 1e4*1./[2^7, 2^6, 2^5, 2^4, 2^3, 2^2, 2^1, 1];
 
 % Normalized cut-off frequencies
 fc_norm = fc/Nyquist;
 
 % Generate coefficients
-win = kaiser(filter_order(1)+1);
+win = hamming(filter_order(1)+1);
 b{1} = fir1(filter_order(1), fc_norm(1), 'low', win);
 for i = 2:8
-    win = kaiser(filter_order(i)+1);
-    b{i} = iir1(filter_order(i), [fc_norm(i-1) fc_norm(i)], 'bandpass', win);
+    win = hamming(filter_order(i)+1);
+    b{i} = fir1(filter_order(i), [fc_norm(i-1) fc_norm(i)], 'bandpass', win);
 end;
 
 % Weigh coefficients
@@ -55,5 +57,10 @@ dB = @(x) 20*log10(abs(x));
 f = w*Nyquist/pi;
 plot(f, dB(h1), f, dB(h2), f, dB(h3), f, dB(h4), ...
     f, dB(h5), f, dB(h6), f, dB(h7), f, dB(h8), f, dB(h_sum));
-axis([0 10e3 -4 8]);
+%axis([0 10e3 -4 8]);
 hold on;
+
+% Calculate frequency response using a specific number of bits
+% for the coefficients
+B = 12;
+
